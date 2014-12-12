0132c4136703458c92b74032d10c61da425b204a
Arif <arif.uisc@gmail.com>
2014-12-11 22:24:20 -0500

    array_search

---
+++ arrayfunction/array_search.php
@@ -0,0 +1,8 @@
+
+
+<?php
+$array = array(0 => 'blue', 1 => 'red', 2 => 'green', 3 => 'red');
+
+$key = array_search('green', $array); // $key = 2;
+$key = array_search('red', $array);   // $key = 1;
+?>
